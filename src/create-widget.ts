cbca953da7c1d111ab5057a30ddd002ad7daa325
SilentDepth <silentdepth@outlook.com>
2023-03-13 20:02:18 +0800

    chore: add Vercel Analytics

--- src/create-widget.ts
+++ src/create-widget.ts
@@ -1,7 +1,10 @@
 import 'uno.css'
 import { createApp, type Component } from 'vue'
+import { inject } from '@vercel/analytics'
 
 import './style.css'
+
+inject()
 
 export default function createWidget (App: Component) {
   const params = Object.fromEntries(new URL(location.href).searchParams.entries())
