33150bdbbfc04dd3dfd6f28fe978b08f8fa741bf
SilentDepth <silentdepth@outlook.com>
2023-03-15 15:13:23 +0800

    chore: avoid any

--- src/create-widget.tsx
+++ src/create-widget.tsx
@@ -1,12 +1,12 @@
 import 'uno.css'
-import React from 'react'
+import React, { type FunctionComponent } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import '~/style.css'
 
 import('@vercel/analytics').then(({ inject }) => inject(), () => {})
 
-export default function createWidget (App: any) {
+export default function createWidget (App: FunctionComponent) {
   const params = Object.fromEntries(new URL(location.href).searchParams.entries())
   ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
     <React.StrictMode>
