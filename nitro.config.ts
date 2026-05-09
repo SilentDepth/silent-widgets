e29c4078c6221a99ee4b68c1d44cc26cbf4bc9a0
SilentDepth <github@silent.land>
2026-05-09 10:22:37 +0800

    🔥 remove vercel

--- nitro.config.ts
+++ nitro.config.ts
@@ -4,14 +4,15 @@ export default defineConfig({
   compatibilityDate: '2026-05-01',
   serverDir: true,
   renderer: false,
-  ...(process.env.VERCEL
-    ? { preset: 'vercel' }
-    : {
-        preset: 'cloudflare-module',
-        cloudflare: {
-          wrangler: {
-            keep_vars: true,
-          },
-        },
-      }),
+  routeRules: {
+    '/': {
+      redirect: 'https://silentdepth.notion.site/SILENT-WIDGETS-8398fc74d82447c687c8b03a08c244c6',
+    },
+  },
+  preset: 'cloudflare-module',
+  cloudflare: {
+    wrangler: {
+      keep_vars: true,
+    },
+  },
 })
